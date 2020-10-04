ab8af401d746af6d98da6a6052f536b01af1aadd
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2020-10-03 22:52:04 -0500

    Connected Areas pages with live data and added styling

--- meal-db/src/router/components/areas/area/index.jsx
+++ meal-db/src/router/components/areas/area/index.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { filterArea } from '../../../../app_state/action_creators/filterActions.jsx';
 
+import { ImageCard } from '../../../../static_components/imageCard.jsx';
+import './index.css';
+
 class Area extends React.Component {
 	constructor(props) {
 		super(props);
@@ -12,32 +15,11 @@ class Area extends React.Component {
 		}
 	}
 	componentDidMount() {
-		console.log(`Area ID: ${this.state.areaID}`);
-		const data = {
-			meals: [
-			    {
-			      strMeal: "BeaverTails",
-			      strMealThumb: "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
-			      idMeal: 52928
-			    },
-			    {
-			      strMeal: "Breakfast Potatoes",
-			      strMealThumb: "https://www.themealdb.com/images/media/meals/1550441882.jpg",
-			      idMeal: 52965
-			    },
-			    {
-			      strMeal: "Canadian Butter Tarts",
-			      strMealThumb: "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
-			      idMeal: 52923
-			    },
-			    {
-			      strMeal: "Montreal Smoked Meat",
-			      strMealThumb: "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
-			      idMeal: 52927
-			    }
-			]
-		}
-		this.props.filterArea(data);
+		fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${this.state.areaID}`)
+			.then(response => response.json())
+			.then(data => {
+				this.props.filterArea(data);
+			});
 	}
 	componentDidUpdate(prevProps,prevState) {
 		if (prevState["meals"].length === 0) {
@@ -51,24 +33,15 @@ class Area extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
+				<h2><ins>{ this.state.areaID }</ins></h2>
+				<div id="flexWrap">
 				{
 					this.state.meals.map(meal =>
-						<div key={meal.idMeal}>
-							<label htmlFor="meal">Meal: </label>
-							<span name="meal">
-								{ meal.strMeal }
-							</span><br />
-							<label htmlFor="id">Id: </label>
-							<span name="id">
-								{ meal.idMeal }
-							</span><br />
-							<label htmlFor="thumbnail">Thumbnail: </label>
-							<span name="thumbnail">
-								{ meal.strMealThumb }
-							</span>
-						</div>
+						<ImageCard key={meal.idMeal} text={meal.strMeal} imageURL={meal.strMealThumb} 
+							imgAlt={meal.strMeal} recipeLink={`/Meals/${meal.idMeal}`} />
 					)
 				}
+				</div>
 			</React.Fragment>
 		)
 	}
