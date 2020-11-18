e82d374b116f7b61ec36ea02c81bf67a74784cbc
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2020-11-17 21:29:51 -0600

    Properly structured files for deployment

---
+++ build/precache-manifest.d9224c0a0a387df5e49e09057096e50c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e950bcf6f63f269d00ed85f5bb6aff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2ada9c72013bf8cd1b9",
+    "url": "/static/css/main.fafac773.chunk.css"
+  },
+  {
+    "revision": "0e0107b733722ac5d904",
+    "url": "/static/js/2.5dc0965c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5dc0965c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ada9c72013bf8cd1b9",
+    "url": "/static/js/main.01b0a093.chunk.js"
+  },
+  {
+    "revision": "52df5ded727cc558e3a4",
+    "url": "/static/js/runtime-main.9e78466b.js"
+  }
+]);
