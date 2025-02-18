b16dc4324f9c52cbc06c1385fb422264ff2cefd5
Joshua Colvin <joshicolvin@gmail.com>
2025-02-17 22:14:14 -0500

    Converted Categories class component into a functional component.

--- src/router/components/categories/index.jsx
+++ src/router/components/categories/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { connect } from 'react-redux';
 import { getCategories } from '../../../app_state/action_creators/getActions.jsx';
@@ -6,19 +6,21 @@ import { getCategories } from '../../../app_state/action_creators/getActions.jsx
 import { NavButton, StyledButton, DescriptionCard } from '../../../static_components';
 import styles from './index.module.css';
 
-class Categories extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLoading: true,
-			categories: []
-		}
-	}
-	componentDidMount() {
+function Categories({ categories, getCategories, history }){
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
 		fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
 			.then(response => response.json())
-			.then(data => this.props.getCategories(data));
-	}
+			.then(data => getCategories(data))
+			.finally(() => setLoading(false));
+
+		return () => {
+			getCategories({categories: []});
+		}
+	},[getCategories]);
+/*
+	Was in previous class component. Appears to not 
 	componentDidUpdate(prevProps, prevState) {
 		if (prevState["categories"].length === 0) {
 			this.setState(function() {
@@ -29,36 +31,32 @@ class Categories extends React.Component {
 			})
 		}
 	}
-	componentWillUnmount() {
-		this.props.getCategories({categories: []});
-	}
-	render() {
-		return (
-			<React.Fragment>
-				<header className={styles.categoriesHeader}>
-					<StyledButton onClickHandler={() => this.props.history.goBack()}>Go Back</StyledButton>
-					<h2><ins>Categories</ins></h2>
-					<NavButton text="Home" path="/Home" />
-				</header>
-				<div className={styles.flexWrap}>
-				{
-					this.state.isLoading ?
-						<>
-							<DescriptionCard header="Loading..." />
-							<DescriptionCard header="Loading..." />
-							<DescriptionCard header="Loading..." />
-							<DescriptionCard header="Loading..." />
-						</> :
-						this.state.categories.map(category => 
-							<DescriptionCard key={category["idCategory"]} header={category["strCategory"]} 
-								imageURL={category["strCategoryThumb"]} imageLink={`/Categories/${category['strCategory']}`}
-								descr={category["strCategoryDescription"]}/>
-						)
-				}
-				</div>
-			</React.Fragment>
-		);
-	}
+*/
+	return (
+		<React.Fragment>
+			<header className={styles.categoriesHeader}>
+				<StyledButton onClickHandler={() => history.goBack()}>Go Back</StyledButton>
+				<h2><ins>Categories</ins></h2>
+				<NavButton text="Home" path="/Home" />
+			</header>
+			<div className={styles.flexWrap}>
+			{
+				loading ?
+					<>
+						<DescriptionCard header="Loading..." />
+						<DescriptionCard header="Loading..." />
+						<DescriptionCard header="Loading..." />
+						<DescriptionCard header="Loading..." />
+					</> :
+					categories.map(category => 
+						<DescriptionCard key={category["idCategory"]} header={category["strCategory"]} 
+							imageURL={category["strCategoryThumb"]} imageLink={`/Categories/${category['strCategory']}`}
+							descr={category["strCategoryDescription"]}/>
+					)
+			}
+			</div>
+		</React.Fragment>
+	);
 }
 
 const mapStateToProps = state => {
