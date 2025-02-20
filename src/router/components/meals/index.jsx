5e00fc0b6af2cfb318aeeb598c016f003a77c9ab
Joshua Colvin <joshicolvin@gmail.com>
2025-02-20 16:19:35 -0500

    Converted Meals class component into a functional component.

--- src/router/components/meals/index.jsx
+++ src/router/components/meals/index.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { connect } from 'react-redux';
 import { filterID } from '../../../app_state/action_creators/filterActions.jsx';
 
 import { RecipeCard } from '../../../static_components';
 
-class Meal extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLoading: true,
-			mealID: props.match.params.mealID,
-			_meal: {}
-		}
-	}
+function Meal({ meal, filterID, match: { params } }) {
+	// Extract out necessary values from parameters
+	const { mealID } = params;
+	// Local state
+	const [loading, setLoading] = useState(true);
+	const [_meal, setMeal] = useState({});
 
-	// Fetch meals
-	componentDidMount() {
-		fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${this.state.mealID}`)
+	useEffect(() => {
+		fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
 			.then(response => response.json())
-			.then(data => {
-				this.props.filterID(data);
-			});
-	}
-	componentDidUpdate(prevProps,prevState) {
+			.then(data => filterID(data))
+			.finally(() => setLoading(false));
+
+		return () => {
+			filterID({meals:[]});
+		}
+	},[filterID,mealID]);
+
+	useEffect(() => {
+		console.log(meal)
 		// Store retrieved meals into local state for formatting 
-		if ((prevProps.meal.length === 0) && (this.props.meal.length === 1)) {
+		if (meal && Object.keys(meal).length > 0) {
 			let i = 1;
-			let _meal = this.props.meal[0];
+			let _meal = meal;
 			let formattedStr = "";
 			let isEmptyStr;
 			let ingredients = [];
@@ -36,54 +37,48 @@ class Meal extends React.Component {
 				ingredient = _meal[`strIngredient${i}`];
 				measurement = _meal[`strMeasure${i}`];
 
-				isEmptyStr = (measurement === "") || (measurement === " ") || (measurement === null);
+				/*
+					measurement?.trim()
+						If measurement is '', ' ', null:
+							measurement becomes '', '', undefined respectively
+					!measurement?.trim()
+						measurement becomes false -> isEmptyStr
+				*/
+				isEmptyStr = !measurement?.trim();
 				formattedStr = ingredient + (isEmptyStr ? "" : ` - ${measurement}`);
 				ingredients.push(formattedStr);
 				i++;
 
-			} while((_meal[`strIngredient${i}`] !== "") && (_meal[`strIngredient${i}`] !== null) && (_meal[`strIngredient${i}`] !== "") && (_meal[`strIngredient${i}`] !== null) && (i <= 20));
+			} while(_meal[`strIngredient${i}`] && (i <= 20));
 
-			this.setState((prevState,currProps) => {
-				let meal = currProps.meal[0];
-				return ({
-					isLoading: false,
-					_meal: {
-						strMeal: meal["strMeal"],
-						strCategory: meal["strCategory"],
-						strArea: meal["strArea"],
-						strInstructions: meal["strInstructions"],
-						strMealThumb: meal["strMealThumb"],
-						strTags: meal["strTags"],
-						strYoutube: meal["strYoutube"],
-						strIngredients: [...ingredients],
-						strSource: meal["strSource"]	
-					}
-				})
-			});
+			setMeal(() => ({
+				..._meal,
+				strMeal: meal["strMeal"],
+				strCategory: meal["strCategory"],
+				strArea: meal["strArea"],
+				strInstructions: meal["strInstructions"],
+				strMealThumb: meal["strMealThumb"],
+				strTags: meal["strTags"],
+				strYoutube: meal["strYoutube"],
+				strIngredients: [...ingredients],
+				strSource: meal["strSource"]
+			}));
 		}
-	}
-	// Cleanup meal state 
-	componenWillUnmount() {
-		filterID({
-			meals:[]
-		});
-	}
+	},[meal]);
 
-	render() {
-		return (
-			<React.Fragment>
-			{
-				this.state.isLoading ?
-					<h2>Loading...</h2> :
-					<RecipeCard {...this.state._meal} btnText="Home" btnPath="/Home" />
-			}
-			</React.Fragment>
-		)
-	}
+	return (
+		<React.Fragment>
+		{
+			loading ?
+				<h2>Loading...</h2> :
+				<RecipeCard {..._meal} btnText="Home" btnPath="/Home" />
+		}
+		</React.Fragment>
+	);
 }
 
 const mapStateToProps = (state) => {
-	return { meal: state.filterMealsReducer }
+	return { meal: state.filterMealsReducer[0] }
 }
 
 export default connect(
