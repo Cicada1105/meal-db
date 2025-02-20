42157ba1e06586ecbdd72a5aa7ab0586052c6e47
Joshua Colvin <joshicolvin@gmail.com>
2025-02-20 17:43:39 -0500

    Converted SearchMeal class component into a functional component.
    Wrapped search input element in newly available <search> tag. Updated styles to reflect adding new tag.

--- src/router/components/meals/search_meal/index.jsx
+++ src/router/components/meals/search_meal/index.jsx
@@ -1,5 +1,4 @@
-//import React, { useEffect, useState, useRef } from 'react';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import { connect } from 'react-redux';
 import { filterName } from '../../../../app_state/action_creators/filterActions.jsx';
@@ -9,72 +8,58 @@ import styles from './index.module.css';
 import { PreviewCard } from './previewCard.jsx';
 import { StyledButton } from '../../../../static_components';
 
+function SearchMeal({ meals, filterName, history }) {
+	const [doneSearching, setDoneSearching] = useState(undefined);
+	const inputRef = useRef(null);
 
-class SearchMeal extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isDoneSearching: undefined
+	useEffect(() => {
+		return () => {
+			filterName({meals: []});
 		}
-		this.inputRef = React.createRef("");
-	}
-	componentWillUnmount() {
-		this.props.filterName({
-			meals: []
-		});
-	}
-	handleClick = () => {
+	},[]);
+
+	const handleClick = () => {
 		// Update loading state
-		this.setState(function() {
-			return {
-				isDoneSearching: false
-			}
-		});
+		setDoneSearching(false);
 		
 		// Get user input
-		let enteredVal = this.inputRef.current.value.toLowerCase();
+		let enteredVal = inputRef.current.value.toLowerCase();
 		// Extract first letter for searching mealdb
 		let firstLetter = enteredVal.slice(0,1);
 		if (firstLetter) {
 			fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`)
 				.then(response => response.json())
-				.then(data =>  {
-					let filteredMeals = data.meals.filter((meal) => meal["strMeal"].toLowerCase().includes(enteredVal));
-					this.setState(() => ({
-						isDoneSearching: true
-					}));
-					this.props.filterName({
-						meals: filteredMeals
-					})
-				})
+				.then(data => 
+					data.meals.filter((meal) => meal["strMeal"].toLowerCase().includes(enteredVal))
+				)
+				.then(filteredMeals => filterName({ meals: filteredMeals }))
+				.finally(() => setDoneSearching(true));
 		}
 	}
-
-	render() {
-		return (
-			<React.Fragment>
-				<header className={styles.searchMealHeader}>
-					<StyledButton onClickHandler={() => this.props.history.goBack()}>Go Back</StyledButton>
-					<h2><ins>Search Meal</ins></h2>
-				</header>
-				<input ref={this.inputRef} type="text" />
-				{/*<div className={styles.searchBtn} onClick={this.handleClick}>Search</div>*/}
-				<StyledButton onClickHandler={this.handleClick}>Search</StyledButton>
-				{
-					this.state.isDoneSearching === undefined ||
+	return (
+		<React.Fragment>
+			<header className={styles.searchMealHeader}>
+				<StyledButton onClickHandler={() => history.goBack()}>Go Back</StyledButton>
+				<h2><ins>Search Meal</ins></h2>
+			</header>
+			<input ref={inputRef} type="text" />
+			<search>
+				<StyledButton onClickHandler={handleClick}>Search</StyledButton>
+			</search>
+			{
+				doneSearching === undefined ||
+				(
+					!doneSearching ?
+					<h3>Loading...</h3> :
 					(
-						!this.state.isDoneSearching ?
-						<h3>Loading...</h3> :
-						(
-							this.props.meals.length !== 0 ?
-							this.props.meals.map((meal,i) => <PreviewCard key={i} {...meal} />) :
-							<h3>Unable to find "{this.inputRef.current.value}"</h3>
-						)
+						meals.length !== 0 ?
+						meals.map((meal,i) => <PreviewCard key={i} {...meal} />) :
+						<h3>Unable to find "{inputRef.current.value}"</h3>
 					)
-				}
-			</React.Fragment>
-		)
-	}
+				)
+			}
+		</React.Fragment>
+	);
 }
 
 const mapStateToProps = state => {
