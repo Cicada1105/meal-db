8e244c2313f00a204b789a0236ed4de021ce012f
Joshua Colvin <joshicolvin@gmail.com>
2025-03-01 19:24:47 -0500

    Implemented proper responsiveness to the home page. Removed NavButton component style width; buttons should be styled with padding or explicitly on individual buttons.

--- src/router/components/home/row.jsx
+++ src/router/components/home/row.jsx
@@ -8,20 +8,20 @@ import cardBg1 from '../../../resources/imgs/brush_stroke_1.png';
 import cardBg2 from '../../../resources/imgs/brush_stroke_2.png';
 
 function Row(props) {
-	const descriptionStyle = {
+	/*const descriptionStyle = {
 		position:"absolute",
 		top:"1.2rem",
 		width:"14rem"
-	}
+	}*/
 	return(
 		<div className={ styles.row }>
-			<div className={ styles.btnCont }>
-				<NavButton path={props.btn.path} text={props.btn.text} />
-			</div>
 			<div className={ styles.cardCont }>
-				<img src={ props.imgNum === 1 ? cardBg1 : cardBg2 } className={ props.imgNum === 1 ? styles.cardBg1 : styles.cardBg2 } alt="*Description Card*" />
-				<div style={descriptionStyle}>{props.descr}</div>
+				<img src={ props.imgNum === 1 ? cardBg1 : cardBg2 } className={ props.imgNum === 1 ? styles.cardBg1 : styles.cardBg2 } alt={ props.btn.text + ' description image background'} aria-hidden="true" />
+				<p>{props.descr}</p>
 			</div>
+			{/*<div className={ styles.btnCont }>*/}
+				<NavButton path={props.btn.path} text={props.btn.text} />
+			{/*{</div>}*/}
 		</div>
 	)
 }
