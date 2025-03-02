9ae6e1bb84b02d791168971f91876cde1aa06d25
Joshua Colvin <joshicolvin@gmail.com>
2025-03-01 20:55:55 -0500

    Removed old commented code in the home page Row component.

--- src/router/components/home/row.jsx
+++ src/router/components/home/row.jsx
@@ -8,20 +8,13 @@ import cardBg1 from '../../../resources/imgs/brush_stroke_1.png';
 import cardBg2 from '../../../resources/imgs/brush_stroke_2.png';
 
 function Row(props) {
-	/*const descriptionStyle = {
-		position:"absolute",
-		top:"1.2rem",
-		width:"14rem"
-	}*/
 	return(
 		<div className={ styles.row }>
 			<div className={ styles.cardCont }>
 				<img src={ props.imgNum === 1 ? cardBg1 : cardBg2 } className={ props.imgNum === 1 ? styles.cardBg1 : styles.cardBg2 } alt={ props.btn.text + ' description image background'} aria-hidden="true" />
 				<p>{props.descr}</p>
 			</div>
-			{/*<div className={ styles.btnCont }>*/}
-				<NavButton path={props.btn.path} text={props.btn.text} />
-			{/*{</div>}*/}
+			<NavButton path={props.btn.path} text={props.btn.text} />
 		</div>
 	)
 }
